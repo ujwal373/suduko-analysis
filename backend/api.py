630b3bf274e87d0c80479d46257a04a03c522b2c
ujwal373 <ujwal.mojidra@gmail.com>
2026-06-03 16:32:08 +0100

    MOD: backend errors fixed

--- backend/api.py
+++ backend/api.py
@@ -345,4 +345,4 @@ async def api_difficulty_color(difficulty: str):
 # ---- Run Server ----
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("api:app", host="0.0.0.0", port=8000, reload=True)
